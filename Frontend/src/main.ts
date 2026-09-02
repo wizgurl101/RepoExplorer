da4ed6ccc6915175e029129634eef44fa94ec723
wizgurl101 <nb.phan@hotmail.com>
2026-09-01 22:08:40 -0600

    change the style

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,10 +1,30 @@
 import { createApp } from "vue";
-import { createVuetify } from "vuetify";
-import "@mdi/font/css/materialdesignicons.css";
-import "vuetify/styles";
-
 import App from "./App.vue";
 
-const vuetify = createVuetify();
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+
+  theme: {
+    defaultTheme: "skillCheckTheme",
+
+    themes: {
+      skillCheckTheme: {
+        colors: {
+          background: "#1D546D",
+          surface: "#F3F4F4",
+          primary: "#061E29",
+          "on-background": "#061E29",
+          "on-surface": "#061E29",
+        },
+      },
+    },
+  },
+});
 
 createApp(App).use(vuetify).mount("#app");
